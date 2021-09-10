a8f9fb41225330503caa222fb1cf9158fa828b1e
Parthsonic <72377707+Parthsonic@users.noreply.github.com>
2021-09-10 11:20:57 +0530

    Delete Smart_Jobs directory

--- Smart_Jobs/src/main/java/com/smart_jobs/repository/CompanyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.smart_jobs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smart_jobs.web.model.Company;
-
-public interface CompanyRepo extends JpaRepository<Company, String> {
-
-}
--- Smart_Jobs/src/main/java/com/smart_jobs/repository/Employer_Repo.java
+++
@@ -1,9 +0,0 @@
-package com.smart_jobs.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smart_jobs.web.model.Employer;
-
-public interface Employer_Repo extends JpaRepository<Employer,Long>{
-
-}
